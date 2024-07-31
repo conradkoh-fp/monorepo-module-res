4c4d96af6e70d4027c49143afd74197332479b26
conradkoh-fp <conrad.koh@foodpanda.com>
2024-07-31 11:48:53 +0800

    initial commit

---
+++ packages/mod1/src/main.ts
@@ -0,0 +1,7 @@
+import { libfunc } from "mod2/src/lib/test";
+
+export function main() {
+  const val = libfunc();
+  console.log({ val });
+}
+main();
---
+++ packages/mod2/src/lib/test.ts
@@ -0,0 +1,4 @@
+import { isEqual } from "lodash";
+export function libfunc() {
+  return isEqual(1, 1);
+}
